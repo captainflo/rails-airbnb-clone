3ae9deafa66362e565a170c883ea808c45657dd6
Yas la menace <yasbernays@gmail.com>
2016-11-14 16:16:35 +0100

    finish modele controller vehicle and category

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,17 +1,28 @@
-git branchclass VehiclesController < ApplicationController
+class VehiclesController < ApplicationController
+before_action :set_vehicle, only: [:show, :edit, :update, :destroy]
   def index
+    @vehicles = Vehicule.all
   end
 
   def show
+
   end
 
   def new
+    @vehicle = Vehicule.new
   end
 
   def create
+    @vehicle = Vehicule.new(vehicle_params)
+    if @vehicle.save
+      redirect_to @vehicle
+    else
+      render :new
+    end
   end
 
   def edit
+
   end
 
   def update
@@ -19,4 +30,15 @@ git branchclass VehiclesController < ApplicationController
 
   def destroy
   end
+
+private
+
+  def set_vehicle
+  @vehicle = Vehicule.find(param[:id])
+  end
+
+  def vehicle_params
+    params.require(:vehicle).permit(:user, :description, :marque, :category, :rating )
+  end
+
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,4 +2,9 @@ class Vehicle < ApplicationRecord
   has_many: :reservations
   belongs_to: :user
   belongs_to: :category
+  validates :user, presence: true
+  validates :description, presence: true
+  validates :marque, presence: true
+  validates :category, presence: true
+
 end

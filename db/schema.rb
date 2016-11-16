1d9f7bafa386b8049a79fb393c4764080398e214
Ronan Louarn <ronan33720@hotmail.Com>
2016-11-16 11:13:53 +0100

    fix categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115143643) do
+ActiveRecord::Schema.define(version: 20161116100942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"

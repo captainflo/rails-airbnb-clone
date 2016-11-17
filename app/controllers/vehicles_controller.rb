fe703b81f3aafd01fd9b946f23f307f2b87f2b7b
Ronan Louarn <ronan33720@hotmail.Com>
2016-11-17 13:37:22 +0100

    Accessiblity of index vehicles ans show without sign in

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,5 +1,6 @@
 class VehiclesController < ApplicationController
 before_action :set_vehicle, only: [:show, :edit, :update, :destroy]
+skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @vehicles = Vehicle.near(params_search[:city], 10)
 

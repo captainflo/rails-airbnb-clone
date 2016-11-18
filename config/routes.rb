bf80671caafb9da97fdcb0cb7e0213e0109d660e
florian lahitte <lahitte.florian@gmail.com>
2016-11-18 17:20:53 +0100

    Dash fixV2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
+get 'dashboard' => 'pages#dashboard'
 
   resources :vehicles do
     resources :reviews
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  get 'dashboard' => 'pages#dashboard'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
